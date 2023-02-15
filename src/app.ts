343858e091ae3341a4a5395f641a9ee537485b0a
aBgAmeuR <a.josset@yahoo.com>
2023-02-15 21:16:08 +0100

    new files

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import express, { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import { logger } from './Config/Output';
 import { config } from './Config/Config';
-import User from './Models/User';
-import mongoose from './DataBase/Database';
 
 const app = express();
 
