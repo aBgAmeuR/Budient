8fcf916b450666924ec5b05c53fdcf842bdfe1ce
aBgAmeuR <a.josset@yahoo.com>
2023-03-10 12:48:22 +0100

    add routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { Application, Request, Response, NextFunction, ErrorRequestHandler } fro
 import { logger } from './Config/Output';
 import { config } from './Config/Config';
 import User from './Routes/User';
+import Transaction from './Routes/Transactions';
+import Auth from './Routes/Auth';
 
 const app: Application = express();
 
@@ -11,7 +13,9 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response): void => {
   res.send('Hello World!');
 });
-app.use(User);
+app.use("api/user", User);
+app.use("api/transaction", Transaction);
+app.use("api/auth", Auth);
 
 app.listen(config.PORT, (): void => {
   logger('Server Running!');
