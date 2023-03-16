bff71c6e43e53855e1d8ca127589af5c12c2e7f8
aBgAmeuR <a.josset@yahoo.com>
2023-03-16 21:31:22 +0100

    setup routes

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Analytics from './pages/Analytics';
+import Categories from './pages/Categories';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+import Subscription from './pages/Subscription';
+import Transactions from './pages/Transactions';
+
+
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    { path: '/dashboard', element: <Dashboard /> },
+    { path: '/analytics', element: <Analytics /> },
+    { path: '/categories', element: <Categories /> },
+    { path: '/profile', element: <Profile /> },
+    { path: '/subscription', element: <Subscription /> },
+    { path: '/transactions', element: <Transactions /> },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './App';
 import './styles/index.scss';
+import App from './App';
 
-const routes = (
-  <Router>
-      <Route path="/" Component={App} />
-  </Router>
-);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>{routes}</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
