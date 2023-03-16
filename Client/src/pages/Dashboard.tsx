5a5e17496cffdc94f2160f3b3d81a60914bdef5c
aBgAmeuR <a.josset@yahoo.com>
2023-03-16 22:26:28 +0100

    add sidebar

--- Client/src/pages/Dashboard.tsx
+++ Client/src/pages/Dashboard.tsx
@@ -1,7 +1,14 @@
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="App">
+      <Header />
+      <Sidebar />
+      <main>
+        <h1>Dashboard</h1>
+      </main>
     </div>
-  )
+  );
 }
