112ea75c5f23fc2698c9c448222866a10c8d328f
aBgAmeuR <a.josset@yahoo.com>
2023-03-19 23:15:18 +0100

    create list

--- Client/src/pages/Transactions.tsx
+++ Client/src/pages/Transactions.tsx
@@ -1,6 +1,76 @@
+import DataTable, { TableColumn } from 'react-data-table-component';
 import ListItem from '../components/TransactionListItem';
+import React from 'react';
+
+type Transaction = {
+  name: string;
+  date: Date;
+  amount: number;
+  category: string;
+  desc: string;
+};
+
+const columns: TableColumn<Transaction>[]= [
+  {
+    name: 'Name',
+    selector: row => row.name,
+  },
+  {
+    name: 'Date',
+    selector: row => row.date.toISOString().substring(0, 10),
+    sortable: true,
+  },
+  {
+    name: 'Amount',
+    selector: row => row.amount,
+    sortable: true,
+  },
+  {
+    name: 'Category',
+    selector: row => row.category,
+    sortable: true,
+  },
+  {
+    name: 'Description',
+    selector: row => row.desc,
+  },
+];
+
+const data = [
+  {
+    name: 'Apple',
+    date: new Date('2020-11-04'),
+    amount: 11.22,
+    category: 'Food',
+    desc: 'Desciption',
+  },
+  {
+    name: 'Amazon',
+    date: new Date('2021-04-06'),
+    amount: 100.03,
+    category: 'Maison',
+    desc: 'Bought some food',
+  },
+  {
+    name: 'Carfour',
+    date: new Date('2021-11-04'),
+    amount: 9.0,
+    category: 'Food',
+    desc: 'Desciption',
+  },
+];
+
+const paginationComponentOptions = {
+  selectAllRowsItem: false,
+  noRowsPerPage: true,
+};
 
 export default function Transactions() {
+  const [filterText, setFilterText] = React.useState('');
+	const filteredItems = data.filter(
+		item => item.name && item.name.toLowerCase().includes(filterText.toLowerCase()),
+	);
+
   return (
     <main id="Transactions">
       <div className="top">
@@ -8,15 +78,8 @@ export default function Transactions() {
         <button>+ Add Transactions</button>
       </div>
       <div className="content">
-        <div className="filter">
-        </div>
-        <td className="list">
-          <tr className="listItem">
-            {/* <ListItem /> */}
-          </tr>
-        </td>
-
+        <DataTable columns={columns} data={filteredItems} pagination paginationComponentOptions={paginationComponentOptions} />
       </div>
     </main>
-  )
+  );
 }
