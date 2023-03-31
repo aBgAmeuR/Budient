746f685de2183bfceeb75727ad8624239e0039bb
aBgAmeuR <a.josset@yahoo.com>
2023-03-31 14:47:18 +0200

    login and fix transaction page

--- Client/src/pages/Connexion.tsx
+++ Client/src/pages/Connexion.tsx
@@ -1,8 +1,28 @@
 export default function Connexion() {
   return (
     <div id="Connexion">
-      <div className="login">
-
+      <div className="left">
+        <div className="login">
+          <h1>Welcome back</h1>
+          <h2>Welcome back! Please enter your details.</h2>
+          <form action="">
+            <label htmlFor="Email">Email</label>
+            <input type="email" name="Email" id="Email" placeholder="Enter your email" required maxLength={64}/>
+            <label htmlFor="Password">Password</label>
+            <input type="password" name="Password" id="Password" placeholder="**********" required maxLength={64}/>
+            <div className="middle">
+              <div className="remember">
+                <input type="checkbox" name="CheckBox" id="CheckBox" />
+                <label htmlFor="CheckBox">Remember for 30 days</label>
+              </div><a href="#">Forgot password</a>
+            </div>
+            <button className="SubmitBtn" type="submit">Sign in</button>
+          </form>
+          <div className="bottom">
+            <p>Don't have an account? </p>
+            <a href="#">Sign up</a>
+          </div>
+        </div>
       </div>
       <div className="image">
         <img src="connexion-white.png" alt="Application preview image" />
