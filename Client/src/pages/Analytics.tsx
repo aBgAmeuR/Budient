c0495d4f83984e79764299637e119eaa121f312b
aBgAmeuR <a.josset@yahoo.com>
2023-04-04 12:51:06 +0200

    analytics setup

--- Client/src/pages/Analytics.tsx
+++ Client/src/pages/Analytics.tsx
@@ -1,7 +1,12 @@
 export default function Analytics() {
   return (
-    <div>
-      <h1>Analytics</h1>
-    </div>
+    <main id="Transactions">
+      <div className="top">
+        <h2>Transactions</h2>
+      </div>
+      <div className="content">
+
+      </div>
+    </main>
   )
 }
