f7b587152659935c644416817ab0b9f069c38718
aBgAmeuR <a.josset@yahoo.com>
2023-09-09 23:32:40 +0200

    add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/page-header';
+import ThemeToggle from '@/components/ui/theme-toggle';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ThemeToggle />
     </>
   );
 }
