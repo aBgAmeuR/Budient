41472d6e4f4288779e1d2d2b8e738f41d38a60fd
aBgAmeuR <a.josset@yahoo.com>
2023-10-10 16:40:59 +0200

    view transactions

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,54 +1,43 @@
 import Header from '@/components/page-header';
 import MainNav from '@/components/main-nav';
 
-import { promises as fs } from 'fs';
-import path from 'path';
-import { z } from 'zod';
-
 import { columns } from '@/components/ui/transactions/columns';
 import { DataTable } from '@/components/ui/transactions/data-table';
-import { taskSchema } from '@/components/ui/data/schema';
 import { getServerSession } from 'next-auth';
 import { options } from '../api/auth/[...nextauth]/options';
 import { redirect } from 'next/navigation';
-
-// Simulate a database read for tasks.
-async function getTasks() {
-  const data = await fs.readFile(path.join(process.cwd(), 'src/components/ui/data/tasks.json'));
-
-  const tasks = JSON.parse(data.toString());
-
-  return z.array(taskSchema).parse(tasks);
-}
+import prisma from '@/app/lib/prisma';
 
 export default async function TaskPage() {
   const session = await getServerSession(options);
   if (!session) redirect('/auth/signin');
 
-
-  const tasks = await getTasks();
-
+  const transactions = await prisma.transaction.findMany({
+    where: {
+      author: {
+        id: session.user.id,
+      },
+    },
+    orderBy: {
+      date: 'desc',
+    },
+    select: {
+      id: true,
+      name: true,
+      date: true,
+      amount: true,
+      category: true,
+      description: true,
+    },
+  });
+  
   return (
     <>
       <Header />
       <MainNav />
-      {/* <main className='w-[calc(100vw_-_250px)] h-[calc(100vh_-_70px)] relative left-[250px] top-[70px]'> */}
       <main>
-        {/* <div className="md:hidden">
-          <Image src="/examples/tasks-light.png" width={1280} height={998} alt="Playground" className="block dark:hidden" />
-          <Image src="/examples/tasks-dark.png" width={1280} height={998} alt="Playground" className="hidden dark:block" />
-        </div> */}
         <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-          {/* <div className="flex items-center justify-between space-y-2">
-            <div>
-              <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
-              <p className="text-muted-foreground">Here&apos;s a list of your tasks for this month!</p>
-            </div>
-            <div className="flex items-center space-x-2">
-              <UserNav />
-            </div>
-          </div> */}
-          <DataTable data={tasks} columns={columns} />
+          <DataTable data={transactions} columns={columns} />
         </div>
       </main>
     </>
--- src/components/ui/data/schema.ts
+++ src/components/ui/data/schema.ts
@@ -3,12 +3,11 @@ import { z } from "zod"
 // We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
 export const taskSchema = z.object({
-  id: z.string(),
   name: z.string(),
-  date: z.string(),
-  amount: z.number(),
+  date: z.date(),
+  amount: z.number().multipleOf(0.01),
   category: z.string(),
-  desc: z.string(),
+  description: z.string().max(160).optional(),
 })
 
 export type Task = z.infer<typeof taskSchema>
